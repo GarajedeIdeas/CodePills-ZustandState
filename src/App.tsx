9f6e91eca9ae3a6c241a788bdabaa722a3d17441
Garaje de Ideas <82031166+GarajedeIdeas@users.noreply.github.com>
2024-02-12 10:42:41 +0100

    add files

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import './App.css'
+import { Formulario } from './components/Formulario';
+import { useAuthStore } from './store'
+
+function isLogged() {
+  const logged = useAuthStore(state => state.logged);
+  return logged;
+}
+
+function App() {
+
+  const logged = useAuthStore(state => state.logged);
+  const user = useAuthStore(state => state.user);
+
+  const changedLogged = useAuthStore(state => state.changeLogged);
+
+  return (
+    <>
+      {user &&
+        <p>{user.username}</p>
+      }
+      <p>{logged ? 'ACTIVO' : 'NO ACTIVO'}</p>
+      <p>{isLogged() ? 'ACTIVO' : 'NO ACTIVO'}</p>
+      <button onClick={changedLogged}>PULSA</button>
+      <Formulario />
+    </>
+  )
+}
+
+export default App
